674d01c500fee8129ef2aae278bb2fcaacbd4f54
Silvio Biasiol <silvio@Silvios-MacBook-Pro.local>
2024-09-20 14:41:31 +0100

    Fix linter

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -56,8 +56,7 @@ export async function GET(req: NextRequest) {
                 height: 630,
             }
         )
-    } catch (e: any) {
-        console.log(`${e.message}`)
+    } catch (e: unknown) {
         return new Response(`Failed to generate the image`, {
             status: 500,
         })
